021aa11b4801f7b539433245244bb8079e9ffe89
Matteo Cacciola <matteo.cacciola@gmail.com>
2024-12-23 18:29:55 +0100

    implementation of Seos Scraper completed parameter to run specific scrapers added to main README file updated

---
+++ scrapers/seos_scraper.py
@@ -0,0 +1,86 @@
+from typing import List, Type, Dict
+from bs4 import Tag
+from pydantic import BaseModel
+
+from scrapers.base_scraper import BaseConfigScraper, BaseScraper
+
+
+class SeosSource(BaseModel):
+    url: str
+    chapters: int
+
+
+class SeosConfig(BaseConfigScraper):
+    sources: List[SeosSource]
+
+
+class SeosScraper(BaseScraper):
+    @property
+    def cookie_selector(self) -> str:
+        return ""
+
+    @property
+    def model_class(self) -> Type[SeosConfig]:
+        """
+        Return the configuration model class.
+
+        Returns:
+            Type[SeosConfig]: The configuration model class.
+        """
+        return SeosConfig
+
+    def scrape(self, model: SeosConfig) -> Dict[str, List[Tag]] | None:
+        """
+        Scrape the source URLs for HTML links.
+
+        Args:
+            model (SeosConfig): The configuration model.
+
+        Returns:
+            Dict[str, List[Tag]]: a dictionary collecting, for each source, the corresponding ResultSet (i.e., a list) or a list of Tag objects containing the tags to the HTML links. If no tag was found, return None.
+        """
+
+        links = {}
+        for source in model.sources:
+            links[source.url] = self.__scrape_source(source)
+
+        return links if links else None
+
+    def __scrape_source(self, source: SeosSource) -> List[Tag]:
+        """
+        Scrape the source URL for HTML links.
+
+        Args:
+            source (SeosSource): The source to scrape.
+
+        Returns:
+            List[Tag]: A list of Tag objects containing the HTML links.
+        """
+        self._logger.info(f"Processing Source {source.url}")
+
+        html_tags = []
+        for i in range(1, source.chapters + 1):
+            self._logger.info(f"Processing Chapter {i}")
+            try:
+                scraper = self._scrape_url(source.url.format(**{"chapter": i}))
+                i_str = f"-c{i}" if i >= 10 else f"-c0{i}"
+
+                html_tags_chapter = scraper.find_all("a", href=lambda href: href and i_str in href)
+                html_tags.extend(html_tags_chapter)
+            except Exception as e:
+                self._logger.error(f"Failed to process Chapter {i}. Error: {e}")
+
+        self._logger.info(f"HTML links found: {len(html_tags)}")
+        return html_tags
+
+    def post_process(self, scrape_output: Dict[str, List[Tag]]) -> List[str]:
+        """
+        Extract the href attribute from the links.
+
+        Args:
+            scrape_output (Dict[str, List[Tag]]): A dictionary collecting, for each source, the corresponding ResultSet (i.e., a list) or a list of Tag objects containing the tags to the HTML links.
+
+        Returns:
+            List[str]: A list of strings containing the HTML links
+        """
+        return [tag.get("href") for tags in scrape_output.values() for tag in tags]
