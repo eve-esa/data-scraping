348a259c2a04c21eac3648412c5bcd364f960cca
Matteo Cacciola <matteo.cacciola@gmail.com>
2025-01-12 00:30:51 +0100

    Wikipedia and MIT sub-scrapers added to Miscellaneous one

--- scraper/miscellaneous_scraper.py
+++ scraper/miscellaneous_scraper.py
@@ -1,6 +1,8 @@
 from typing import List, Dict, Type
 from bs4 import ResultSet, Tag
+from selenium.webdriver.common.by import By
 
+from helper.utils import get_scraped_url
 from model.base_mapped_models import BaseMappedUrlSource
 from scraper.base_mapped_publisher_scraper import BaseMappedPublisherScraper
 from scraper.base_scraper import BaseMappedScraper
@@ -12,6 +14,7 @@ class MiscellaneousScraper(BaseMappedPublisherScraper):
     def mapping(self) -> Dict[str, Type[BaseMappedScraper]]:
         return {
             "OpenNightLightsScraper": OpenNightLightsScraper,
+            "WikipediaScraper": WikipediaScraper,
         }
 
 
@@ -34,6 +37,57 @@ class OpenNightLightsScraper(BaseUrlPublisherScraper, BaseMappedScraper):
 
             self._logger.info(f"HTML links found: {len(html_tag_list)}")
             return html_tag_list
+        except Exception as e:
+            self._logger.error(f"Failed to process Issue / Collection {source.url}. Error: {e}")
+            return None
+
+    def _scrape_article(self, source: BaseMappedUrlSource) -> Tag | None:
+        pass
+
+
+class WikipediaScraper(BaseUrlPublisherScraper, BaseMappedScraper):
+    def _scrape_journal(self, source: BaseMappedUrlSource) -> ResultSet | List[Tag] | None:
+        pass
+
+    def _scrape_issue_or_collection(self, source: BaseMappedUrlSource) -> List[Tag] | None:
+        self._logger.info(f"Processing Issue / Collection {source.url}")
+
+        try:
+            self._scrape_url(source.url)
+
+            html_tag_list = self._driver.find_elements(By.CSS_SELECTOR, "div.mw-category-generated a")
+
+            self._logger.info(f"HTML links found: {len(html_tag_list)}")
+            return [Tag(name="a", attrs={"href": tag.get("href")}) for tag in html_tag_list]
+        except Exception as e:
+            self._logger.error(f"Failed to process Issue / Collection {source.url}. Error: {e}")
+            return None
+
+    def _scrape_article(self, source: BaseMappedUrlSource) -> Tag | None:
+        pass
+
+
+class MITScraper(BaseUrlPublisherScraper, BaseMappedScraper):
+    def _scrape_journal(self, source: BaseMappedUrlSource) -> ResultSet | List[Tag] | None:
+        pass
+
+    def _scrape_issue_or_collection(self, source: BaseMappedUrlSource) -> List[Tag] | None:
+        self._logger.info(f"Processing Issue / Collection {source.url}")
+
+        try:
+            scraper = self._scrape_url(source.url)
+
+            pdf_tag_list = [
+                pdf_tag for tag in scraper.find_all(
+                    "a", href=lambda href: href and "/courses/" in href and "/resources/earthsurface_" in href
+                )
+                if (pdf_tag := self._scrape_url(get_scraped_url(tag, self.base_url)).find(
+                    "a", href=lambda href: href and ".pdf" in href, class_="download-file"
+                ))
+            ]
+
+            self._logger.info(f"PDF links found: {len(pdf_tag_list)}")
+            return pdf_tag_list
         except Exception as e:
             self._logger.error(f"Failed to process Issue / Collection {source.url}. Error: {e}")
             return None
