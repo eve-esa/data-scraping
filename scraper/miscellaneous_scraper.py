bd9bef48dfd08482fa7822ac351424ebd97e4b0a
Matteo Cacciola <matteo.cacciola@gmail.com>
2025-01-12 01:17:36 +0100

    UK Met Office sub-scraper added to Miscellaneous one

--- scraper/miscellaneous_scraper.py
+++ scraper/miscellaneous_scraper.py
@@ -1,5 +1,7 @@
 from typing import List, Dict, Type
 from bs4 import ResultSet, Tag
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 from helper.utils import get_scraped_url
@@ -15,6 +17,9 @@ class MiscellaneousScraper(BaseMappedPublisherScraper):
         return {
             "OpenNightLightsScraper": OpenNightLightsScraper,
             "WikipediaScraper": WikipediaScraper,
+            "MITScraper": MITScraper,
+            "JAXAScraper": JAXAScraper,
+            "UKMetOfficeScraper": UKMetOfficeScraper,
         }
 
 
@@ -94,3 +99,77 @@ class MITScraper(BaseUrlPublisherScraper, BaseMappedScraper):
 
     def _scrape_article(self, source: BaseMappedUrlSource) -> Tag | None:
         pass
+
+
+class JAXAScraper(BaseUrlPublisherScraper, BaseMappedScraper):
+    def _scrape_journal(self, source: BaseMappedUrlSource) -> ResultSet | List[Tag] | None:
+        pass
+
+    def _scrape_issue_or_collection(self, source: BaseMappedUrlSource) -> ResultSet | None:
+        self._logger.info(f"Processing Issue / Collection {source.url}")
+
+        try:
+            scraper = self._scrape_url(source.url)
+
+            # Find all PDF links using appropriate class or tag (if lambda returns True, it will be included in the list)
+            html_tag_list = scraper.find_all("a", href=True, class_="btn--outline")
+
+            self._logger.info(f"HTML links found: {len(html_tag_list)}")
+            return html_tag_list
+        except Exception as e:
+            self._logger.error(f"Failed to process Issue / Collection {source.url}. Error: {e}")
+            return None
+
+    def _scrape_article(self, source: BaseMappedUrlSource) -> Tag | None:
+        pass
+
+
+class UKMetOfficeScraper(BaseUrlPublisherScraper, BaseMappedScraper):
+    def _scrape_journal(self, source: BaseMappedUrlSource) -> ResultSet | List[Tag] | None:
+        pass
+
+    def _scrape_issue_or_collection(self, source: BaseMappedUrlSource) -> List[Tag] | None:
+        self._logger.info(f"Processing Issue / Collection {source.url}")
+
+        try:
+            self._scrape_url(source.url)
+
+            pdf_tag_list = []
+
+            page_buttons = self._driver.find_elements(By.CSS_SELECTOR, "a.role-button.page-link")
+            for page_button in page_buttons:
+                page_button.click()
+                self.__wait_for_loader_hidden()
+
+                scraper = self._get_parsed_page_source()
+                pdf_tag_list.extend(scraper.find_all("a", href=True, class_="card-link-value"))
+
+            self._logger.info(f"PDF links found: {len(pdf_tag_list)}")
+            return pdf_tag_list
+        except Exception as e:
+            self._logger.error(f"Failed to process Issue / Collection {source.url}. Error: {e}")
+            return None
+
+    def _scrape_article(self, source: BaseMappedUrlSource) -> Tag | None:
+        pass
+
+    def __wait_for_loader_hidden(self, timeout: int | None = 10) -> bool:
+        """
+        Using self._driver, wait until the parent of the parent of div.loader-admin has style display: none
+
+        Args:
+            timeout (int | None): The maximum time to wait for the loader to be hidden
+
+        Returns:
+            bool: True if the loader is hidden, False otherwise
+        """
+
+        try:
+            loader = self._driver.find_element(By.ID, "loading-overflow")
+
+            WebDriverWait(self._driver, timeout).until(
+                lambda x: "display: none" in loader.get_attribute("style")
+            )
+            return True
+        except:
+            return False
