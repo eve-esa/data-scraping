a751e93784dddb110fb845c258c5af350e07caba
Matteo Cacciola <matteo.cacciola@gmail.com>
2025-01-27 18:51:06 +0100

    log overwrite

--- helper/logger.py
+++ helper/logger.py
@@ -21,7 +21,7 @@ def setup_logger(name: str, log_file: str = "scraping.log"):
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
     # File handler
-    file_handler = logging.FileHandler(log_file)
+    file_handler = logging.FileHandler(log_file, mode="w")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
