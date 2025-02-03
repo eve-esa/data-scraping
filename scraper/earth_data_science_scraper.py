2240ce6d056446f2f13b92819de5b8c8db249301
Matteo Cacciola <matteo.cacciola@gmail.com>
2025-02-03 02:13:21 +0100

    new scrapers

---
+++ scraper/earth_data_science_scraper.py
@@ -0,0 +1,54 @@
+from typing import List
+from bs4 import ResultSet, Tag
+
+from scraper.base_url_publisher_scraper import BaseUrlPublisherSource, BaseUrlPublisherScraper
+
+
+class EarthDataScienceScraper(BaseUrlPublisherScraper):
+    def _scrape_journal(self, source: BaseUrlPublisherSource) -> ResultSet | List[Tag] | None:
+        """
+        Scrape all articles of a journal.
+
+        Args:
+            source (BaseUrlPublisherSource): The journal to scrape.
+
+        Returns:
+            ResultSet | List[Tag] | None: A ResultSet (i.e., a list) or a list of Tag objects containing the PDF links. If no tag was found, return None.
+        """
+        pass
+
+    def _scrape_issue_or_collection(self, source: BaseUrlPublisherSource) -> ResultSet | None:
+        """
+        Scrape the issue (or collection) URL for PDF links.
+
+        Args:
+            source (BaseUrlPublisherSource): The issue / collection to scrape.
+
+        Returns:
+            ResultSet | None: A ResultSet (i.e., list) object containing the tags to the PDF links, or None if no tag was found.
+        """
+        self._logger.info(f"Processing Issue / Collection {source.url}")
+
+        try:
+            scraper = self._scrape_url(source.url)
+
+            # Find all PDF links using appropriate class or tag (if lambda returns True, it will be included in the list)
+            html_tag_list = scraper.find_all("a", href=lambda href: href and "/courses/" in href)
+
+            self._logger.debug(f"HTML links found: {len(html_tag_list)}")
+            return html_tag_list
+        except Exception as e:
+            self._logger.error(f"Failed to process Issue / Collection {source.url}. Error: {e}")
+            return None
+
+    def _scrape_article(self, source: BaseUrlPublisherSource) -> Tag | None:
+        """
+        Scrape a single article.
+
+        Args:
+            source (BaseUrlPublisherSource): The article to scrape.
+
+        Returns:
+            Tag | None: The tag containing the PDF link found in the article, or None if no tag was found.
+        """
+        pass
