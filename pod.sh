376aac838c5386d9ae2d8f3b2b807ddc5aba9209
Matteo Cacciola <matteo.cacciola@gmail.com>
2025-02-12 18:50:17 +0100

    changes into the script to run within a POD

--- pod.sh
+++ pod.sh
@@ -26,11 +26,12 @@ if ! command -v python3.10 &> /dev/null; then
     echo "Python 3.10 not found. Installing..."
     add-apt-repository -y ppa:deadsnakes/ppa
     apt-get update
-    apt-get install -y python3.10 python3.10-venv python3.10-dev
+    apt-get install -y python3.10 python3.10-venv python3.10-dev python3.10-apt
 fi
 
 # Update the alternatives for Python, PIP, and other tools, in order to use Python 3.10
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+ln -s /usr/lib/python3/dist-packages/apt_pkg.cpython-3*-x86_64-linux-gnu.so /usr/local/lib/python3.10/site-packages/apt_pkg.so
 
 # Verify Python version
 PYTHON_VERSION=$(python3 --version)
